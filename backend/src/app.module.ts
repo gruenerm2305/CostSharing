96d2acd74ff4cb67f48742247c0f0dd4c26337be
Tim Schwegler <schweglert.tin23@student.dhbw-heidenheim.de>
2026-04-03 15:59:25 +0200

    Adding Modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ReceiptsModule } from './receipts/receipts.module';
+import { OcrModule } from './ocr/ocr.module';
+import { SplittingModule } from './splitting/splitting.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -15,6 +20,11 @@ import { UsersModule } from './users/users.module';
       logging: process.env.NODE_ENV === 'development',
     }),
     UsersModule,
+    AuthModule,
+    CategoriesModule,
+    ReceiptsModule,
+    OcrModule,
+    SplittingModule
   ],
 })
 export class AppModule {}
